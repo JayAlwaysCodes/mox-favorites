a9aa7a0e855f2ec13d2a13ef7784c1f5d7648cd6
jayalwayscodes <johnsonjardocs@gmail.com>
2025-03-12 20:49:04 +0100

    added zksync network

--- script/deploy.py
+++ script/deploy.py
@@ -2,25 +2,22 @@ from src import favorites
 from moccasin.boa_tools import VyperContract
 from moccasin.config import get_active_network
 
+
 def deploy_favorites() -> VyperContract:
-    print("Deploying...")
-    favorites_contract: VyperContract = favorites.deploy()
-    starting_number: int = favorites_contract.retrieve()
-    print(f"Starting number: {starting_number}")
-
-    favorites_contract.store(42)
-    new_number: int = favorites_contract.retrieve()
-    print(f"New number: {new_number}")
-
     active_network = get_active_network()
+    print("Currently on network: ", active_network.name)
+
+    favorites_contract: VyperContract = favorites.deploy()
+    print("Starting favorite number: ", favorites_contract.retrieve())
+    favorites_contract.store(77)
+    print("Ending favorite number: ", favorites_contract.retrieve())
 
     if active_network.has_explorer():
+        print("Verifying contract on explorer...")
         result = active_network.moccasin_verify(favorites_contract)
         result.wait_for_verification()
-
     return favorites_contract
 
 
-def moccasin_main():
-    deploy_favorites()
-    print("Deployed!")
+def moccasin_main() -> VyperContract:
+    return deploy_favorites()
